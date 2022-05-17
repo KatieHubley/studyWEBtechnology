612975412d44213a9ecb2d18d3fed010f123452b
KatieHubley <katierhubley@gmail.com>
2022-05-17 08:59:01 -0400

    UploadFinalSpring22
    UploadFinal

---
+++ Spring2022/Spring22Final/15_Carlos/FinalProject.js
@@ -0,0 +1,17 @@
+/*
+  Thanlks for fixes @tripu
+  https://codepen.io/nucliweb/details/eJPeYV#comment-id-226740
+*/
+
+
+
+$(function() {
+  new Typed('#typed', {
+		strings: ["SOCIAL MEDIA","NETWORK","SEARCH ENGINE","INSTAGRAM", "FACEBOOK", "TWITTER", "VIEWS", "CONNECTIONS",],
+		typeSpeed: 100,
+    backDelay: 1500,
+    loop: true,
+    callback: function(){}
+	});
+});
+ // JavaScript Document
